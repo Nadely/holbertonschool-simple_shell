c8a2912cc9b9bb79b7752ab00a66732ada51f01a
ErwanR-Holberton <6218@holbertonstudents.com>
2023-08-16 23:36:54 +0200

    NE - betty clean

--- f_get_path.c
+++ f_get_path.c
@@ -1,23 +1,22 @@
 #include "main.h"
 /**
  * *get_path - get a path where the command can be found
+ * @ev: environment variables
  * @command: the command we are searching for
  * Return: a pointer to the path or null (failure)
  */
-char *get_path(char *command, char **env)
+char *get_path(char *command, char **ev)
 {
 	char *path = NULL, *result = NULL, *token = NULL;
 	int i, res_len = 0;
 	struct stat file_info;
 
-	/*if (stat(command, &file_info) == 0)
-		return (strdup(command));*/
 	if (command[0] == '/' || (command[0] == '.'))
 		if (stat(command, &file_info) == 0)
 			return (strdup(command));
-	for (i = 0; env[i]; i++)
-		if (env[i][0] == 'P' && env[i][1] == 'A' && env[i][2] == 'T' && env[i][4] == '=')
-			path = strdup(env[i]);
+	for (i = 0; ev[i]; i++)
+		if (ev[i][0] == 'P' && ev[i][1] == 'A' && ev[i][2] == 'T' && ev[i][4] == '=')
+			path = strdup(ev[i]);
 	if (path != NULL)
 	{
 		token = strtok(path, "=");
