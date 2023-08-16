a3c79b188fd4e8c6ef0335bde19624668917991c
ErwanR-Holberton <6218@holbertonstudents.com>
2023-08-16 21:59:30 +0200

    EN - try to find direct path

--- f_get_path.c
+++ f_get_path.c
@@ -12,30 +12,35 @@ char *get_path(char *command, char **env)
 
 	/*if (stat(command, &file_info) == 0)
 		return (strdup(command));*/
+	if (command[0] == '/')
+		if (stat(command, &file_info) == 0)
+			return (strdup(command));
 	for (i = 0; env[i]; i++)
 		if (env[i][0] == 'P' && env[i][1] == 'A' && env[i][2] == 'T' && env[i][4] == '=')
 			path = strdup(env[i]);
-
-	token = strtok(path, "=");
-	token = strtok(NULL, ":");
-	while (token != NULL) /* counts the number of arguments for malloc*/
+	if (path != NULL)
 	{
-		res_len = strlen(token) + strlen(command) + 2;
-		result = malloc(res_len);
-		for (i = 0; i < res_len; i++)
-			result[i] = 0;
-
-		strcat(result, token);
-		strcat(result, "/");
-		strcat(result, command);
-		if (stat(result, &file_info) == 0)
-		{
-			free(path);
-			return (result);
-		}
-		free(result);
-		result = NULL;
+		token = strtok(path, "=");
 		token = strtok(NULL, ":");
+		while (token != NULL) /* counts the number of arguments for malloc*/
+		{
+			res_len = strlen(token) + strlen(command) + 2;
+			result = malloc(res_len);
+			for (i = 0; i < res_len; i++)
+				result[i] = 0;
+
+			strcat(result, token);
+			strcat(result, "/");
+			strcat(result, command);
+			if (stat(result, &file_info) == 0)
+			{
+				free(path);
+				return (result);
+			}
+			free(result);
+			result = NULL;
+			token = strtok(NULL, ":");
+		}
 	}
 	free(path);
 	return (0);
